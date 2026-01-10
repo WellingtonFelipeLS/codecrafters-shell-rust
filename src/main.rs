649ae2023d578b5965cef93514319c9315c7d2f1
WellingtonFelipeLS <wefeliso@gmail.com>
2026-01-10 15:16:05 -0300

    Add loop evaluation

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,14 @@ use std::io::{self, Write};
 fn main() {
     let mut buffer = String::new();
 
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let _ = io::stdin().read_line(&mut buffer);
+        let _ = io::stdin().read_line(&mut buffer);
 
-    println!("{}: command not found", buffer.trim());
+        println!("{}: command not found", buffer.trim());
+
+        buffer.clear();
+    }
 }
