949dc590458368794e6aa30e83bab61f7628f5c1
WellingtonFelipeLS <wefeliso@gmail.com>
2026-01-10 15:44:08 -0300

    Implementing type builtin

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io::{self, Write};
 fn main() {
     let mut buffer = String::new();
     let mut user_inputs: Vec<&str>;
+    let builtins = ["exit", "echo", "type"];
 
     loop {
         buffer.clear();
@@ -18,6 +19,17 @@ fn main() {
         match user_inputs.first() {
             Some(&"exit") => return,
             Some(&"echo") => println!("{}", user_inputs[1..].join(" ")),
+            Some(&"type") => {
+                if let Some(command) = user_inputs.get(1) {
+                    if builtins.contains(command) {
+                        println!("{command} is a shell builtin")
+                    } else {
+                        println!("{command}: not found")
+                    }
+                } else {
+                    println!(": not found")
+                }
+            }
             Some(x) => println!("{x}: command not found"),
             _ => println!(": command not found"),
         }
