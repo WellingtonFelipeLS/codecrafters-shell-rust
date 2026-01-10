2e796228c61baaca16083f5121353ae6b903fe83
WellingtonFelipeLS <wefeliso@gmail.com>
2026-01-10 15:11:40 -0300

    Read and display user input

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@
 use std::io::{self, Write};
 
 fn main() {
+    let mut buffer = String::new();
+
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let _ = io::stdin().read_line(&mut buffer);
+
+    println!("{}: command not found", buffer.trim());
 }
